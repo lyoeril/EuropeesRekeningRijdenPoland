86760b2fabcda02ed2f134303851b396d637b7d9
Robin de Kort <Robindekort@hotmail.com>
2018-04-24 13:53:39 +0200

    init

---
+++ TrafficSimulator/src/main/java/eu/poland/trafficsimulator/Main.java
@@ -0,0 +1,11 @@
+package eu.poland.trafficsimulator;
+
+/**
+ *
+ * @author Robin
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
