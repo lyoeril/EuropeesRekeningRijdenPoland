e3643415e40485e114621c376938e1db6511d015
Vries,Laurent L.S.G. de <laurent.devries@student.fontys.nl>
2018-05-08 09:47:37 +0200

    Added invoiceservice funtionality for invoices, fixed methods for IInvoiceDAO from String Month to Date date

--- Rekeningadministratie_Overheid/src/main/java/services/InvoiceService.java
+++ Rekeningadministratie_Overheid/src/main/java/services/InvoiceService.java
@@ -5,10 +5,58 @@
  */
 package services;
 
+import dao.JPA.KMRateJPA;
+import dao.interfaces.IInvoiceDAO;
+import domain.Invoice;
+import domain.Rekeningrijder;
+import domain.Vehicle;
+import java.util.Date;
+import java.util.List;
+import javax.inject.Inject;
+
 /**
  *
  * @author Laurent
  */
 public class InvoiceService {
     
+   @Inject
+   IInvoiceDAO invoiceDAO;
+    
+    public InvoiceService(){
+        
+    }
+    
+    public void addInvoice(Invoice invoice){
+        invoiceDAO.create(invoice);
+    }
+    
+    public void updateInvoice(Invoice invoice){
+        invoiceDAO.edit(invoice);
+    }
+    
+    public void deleteInvoice(Invoice invoice){
+        invoiceDAO.delete(invoice);
+    }
+    
+    public Invoice findInvoiceById(long id){
+        return invoiceDAO.findById(id);
+    }
+    
+    public List<Invoice> findInvoicesByRekeningrijder(Rekeningrijder rekeningrijder){
+        return invoiceDAO.findByRekeningrijder(rekeningrijder);
+    }
+    
+    public List<Invoice> findInvoiceByRekeningrijderMonth(Rekeningrijder rekeningrijder, Date date){
+        return invoiceDAO.findByRekeningrijderMonth(rekeningrijder, date);
+    }
+    
+    public List<Invoice> findInvoiceVehicleMonth(Rekeningrijder rekeningrijder, Date date, Vehicle vehicle){
+        return invoiceDAO.findByVehicleMonth(rekeningrijder, date, vehicle);
+    }
+    
+    private Invoice calculateInvoice(Rekeningrijder rekeningrijder, Date date, Vehicle vehicle){
+        return new Invoice(1, 1, new Date());
+    }
 }
+
