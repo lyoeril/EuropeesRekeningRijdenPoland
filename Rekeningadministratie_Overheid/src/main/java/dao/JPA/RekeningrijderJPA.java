d8e2896fabf6353d0bde1718e42cc8145c7e56e6
Vries,Laurent L.S.G. de <laurent.devries@student.fontys.nl>
2018-05-08 10:50:38 +0200

    implemented base JPA for Rekeningrijder

--- Rekeningadministratie_Overheid/src/main/java/dao/JPA/RekeningrijderJPA.java
+++ Rekeningadministratie_Overheid/src/main/java/dao/JPA/RekeningrijderJPA.java
@@ -5,10 +5,51 @@
  */
 package dao.JPA;
 
+import dao.interfaces.IRekeningrijderDAO;
+import domain.Rekeningrijder;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 /**
  *
  * @author Laurent
  */
-public class RekeningrijderJPA {
-    
+public class RekeningrijderJPA implements IRekeningrijderDAO{
+
+    private static final Logger LOGGER = Logger.getLogger(RekeningrijderJPA.class.getName());
+
+    @PersistenceContext(unitName = "TODO")
+    private EntityManager em;
+
+    public RekeningrijderJPA() {
+    }
+
+    public RekeningrijderJPA(EntityManager em) {
+        this.em = em;
+    }
+
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public void create(Rekeningrijder rekeningrijder) {
+        em.persist(rekeningrijder);
+    }
+
+    @Override
+    public void edit(Rekeningrijder rekeningrijder) {
+        em.merge(rekeningrijder);
+    }
+
+    @Override
+    public void delete(Rekeningrijder rekeningrijder) {
+        em.remove(rekeningrijder);
+    }
+
+    @Override
+    public Rekeningrijder findById(long id) {
+        return em.find(Rekeningrijder.class, id);
+    }
 }
