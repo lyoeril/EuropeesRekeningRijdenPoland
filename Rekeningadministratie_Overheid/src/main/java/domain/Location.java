c7d1a2bc78ee5c425da5e18bd63a889c67f79134
Vries,Laurent L.S.G. de <laurent.devries@student.fontys.nl>
2018-05-15 09:56:35 +0200

    Fixed invoice date + fixed location domain

--- Rekeningadministratie_Overheid/src/main/java/domain/Location.java
+++ Rekeningadministratie_Overheid/src/main/java/domain/Location.java
@@ -5,10 +5,59 @@
  */
 package domain;
 
+import java.util.Calendar;
+
 /**
  *
  * @author Laurent
  */
 public class Location {
+    private long id;
+    private Calendar date;
+    private double latitude;
+    private double longitude;
+    
+    public Location(){
+        
+    }
+
+    public Location(long id, Calendar date, double latitude, double longitude) {
+        this.date = date;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+    
     
 }
