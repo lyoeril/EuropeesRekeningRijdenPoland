a7f9e2c2edb1d01b9af93c87811188affd260de0
Vries,Laurent L.S.G. de <laurent.devries@student.fontys.nl>
2018-05-29 11:56:21 +0200

    cors changed ipaddress to *

--- Rekeningadministratie_Overheid/src/main/java/rest/CorsResponseFilter.java
+++ Rekeningadministratie_Overheid/src/main/java/rest/CorsResponseFilter.java
@@ -21,7 +21,8 @@ public class CorsResponseFilter implements ContainerResponseFilter{
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:4200");
+        //responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:4200");
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
         responseContext.getHeaders().add("Access-Control-Allow-Headers",
                 "Origin, Access-Control-Allow-Origin, Content-Type, Accept, Authorization");
     }
