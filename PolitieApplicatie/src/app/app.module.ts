bd8238af868bdeb0d81cf2d2ea3f5ccf556ccf45
Robin de Kort <Robindekort@hotmail.com>
2018-06-12 15:32:40 +0200

    the great cleansing

--- PolitieApplicatie/src/app/app.module.ts
+++ PolitieApplicatie/src/app/app.module.ts
@@ -8,15 +8,10 @@ import { HttpModule } from '@angular/http';
 import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { InvoiceListComponent } from './invoice-list/invoice-list.component';
 import { VehicleListComponent } from './vehicle-list/vehicle-list.component';
-import { CartrackerComponent } from './cartracker/cartracker.component';
-import { NewCartrackerComponent } from './new-cartracker/new-cartracker.component';
-import { CartrackerListComponent } from './cartracker-list/cartracker-list.component';
 
 import { HttpService } from './_service/http.service';
 import { AuthService } from './_service/auth.service';
@@ -25,15 +20,10 @@ import { AuthService } from './_service/auth.service';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     HeaderComponent,
     LoginComponent,
     NotFoundComponent,
-    InvoiceListComponent,
-    VehicleListComponent,
-    CartrackerComponent,
-    NewCartrackerComponent,
-    CartrackerListComponent
+    VehicleListComponent
   ],
   imports: [
     BrowserModule,
--- PolitieApplicatie/src/app/app.routing.ts
+++ PolitieApplicatie/src/app/app.routing.ts
@@ -1,52 +1,59 @@
-import { ModuleWithProviders } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {ModuleWithProviders} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { LoginComponent } from './login/login.component';
-import { AuthService } from './_service/auth.service';
-import { InvoiceListComponent } from './invoice-list/invoice-list.component';
-import { VehicleListComponent } from './vehicle-list/vehicle-list.component';
-import { CartrackerComponent } from './cartracker/cartracker.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {LoginComponent} from './login/login.component';
+import {AuthService} from './_service/auth.service';
+import {VehicleListComponent} from './vehicle-list/vehicle-list.component';
 
 const routes: Routes = [
-    {
-        path: '',
-        component: InvoiceListComponent,
-        canActivate: [AuthService]
-    },
-    // {
-    //     path: 'home',
-    //     component: HomeComponent,
-    //     canActivate: [AuthService]
-    // },
-    {
-        path: 'invoices',
-        component: InvoiceListComponent,
-        canActivate: [AuthService]
-    },
-    {
-        path: 'vehicles',
-        component: VehicleListComponent,
-        canActivate: [AuthService]
-    },
-    {
-        path: 'cartracker',
-        component: CartrackerComponent,
-        canActivate: [AuthService]
-    },
-    {
-        path: 'login',
-        component: LoginComponent
-    },
-    {
-        path: '404',
-        component: NotFoundComponent
-    },
-    {
-        path: '**',
-        redirectTo: '/404'
-    }
+  {
+    path: '',
+    redirectTo: 'vehicles',
+    pathMatch: 'full'
+  },
+  // {
+  //     path: 'home',
+  //     component: HomeComponent,
+  //     canActivate: [AuthService]
+  // },
+  /*{
+    path: 'invoices',
+    component: InvoiceListComponent,
+    canActivate: [AuthService]
+  },*/
+  {
+    path: 'vehicles',
+    component: VehicleListComponent,
+    canActivate: [AuthService]
+  },
+  {
+    path: 'vehicles/:id',
+    component: VehicleListComponent,
+    canActivate: [AuthService]
+  },
+  {
+    path: 'vehicles/:id/track',
+    component: VehicleListComponent,
+    canActivate: [AuthService]
+  },
+  /*{
+    path: 'cartracker',
+    component: CartrackerComponent,
+    canActivate: [AuthService]
+  },*/
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '404',
+    component: NotFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/404'
+  }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
