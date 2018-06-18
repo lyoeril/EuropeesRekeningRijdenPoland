04d3be28611cfbd4594786c8d749e511aed33333
YoeriVink <y.vink@student.fontys.nl>
2018-06-18 11:56:40 +0200

    Version up

--- RegistratieCentrale/src/main/java/com/poland/dao/implementations/LocationDAOImpl.java
+++ RegistratieCentrale/src/main/java/com/poland/dao/implementations/LocationDAOImpl.java
@@ -58,7 +58,6 @@ public class LocationDAOImpl extends BasicDAOImpl<Location> implements LocationD
             query.setParameter("longitude", longitude);
             query.setParameter("authorisationCode", authorisationCode);
             query.execute();
-//            long var = (long) query.getOutputParameterValue("succeeded");
             return true;
         } catch (Exception ise) {
             handleExceptions(ise);
